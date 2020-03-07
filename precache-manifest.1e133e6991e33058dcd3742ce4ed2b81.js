6e2238104f809c354c90e6f3cca2ad93f949b9d8
27ugeen <27ugeen@gmail.com>
2020-03-07 15:57:23 +0200

    Updates

---
+++ precache-manifest.1e133e6991e33058dcd3742ce4ed2b81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007bad56385a5aeb0104266f970ea0de",
+    "url": "/goit-react-hw-05-phonebook-ctx/index.html"
+  },
+  {
+    "revision": "52e0fc74504426903932",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/css/main.e49d5b04.chunk.css"
+  },
+  {
+    "revision": "ac86091f3aa8c630aa00",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.1faf764e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.1faf764e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52e0fc74504426903932",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/main.d24c0329.chunk.js"
+  },
+  {
+    "revision": "471224084cf842a1de0a",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/runtime-main.0181941c.js"
+  }
+]);
